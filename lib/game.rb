424a9f4ba6e3c5bcf86934cba4286535f570a497
Michael Roose <mjroose@gmail.com>
2018-08-22 00:57:59 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -2,9 +2,9 @@ class Game
   WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
   attr_accessor :board, :player_1, :player_2
 
-  def initialize(player_1 = Players::Human.new, player_2 = Players::Human.new, board = Board.new)
+  def initialize(player_1 = Players::Human.new, player_2 = Players::Human.new)
     @player_1 = player_1
     @player_2 = player_2
-    @board = board
+    @board = Board.new
   end
 end
