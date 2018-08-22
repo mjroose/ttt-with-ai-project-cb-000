0d80b91cc0519ccbbc4c6844a4dc37f7e07a5318
Michael Roose <mjroose@gmail.com>
2018-08-22 00:23:56 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,12 @@ class Board
   def reset!
     self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
+
+  def display
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
 end
